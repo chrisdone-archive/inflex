50a8d761cc6cf6541a0e4ff02524304a27363922
Chris Done <chrisdone@gmail.com>
2020-02-23 23:50:49 +0000

    Easy auto-recompile and restart

--- inflex-client/src/Inflex/Client.hs
+++ inflex-client/src/Inflex/Client.hs
@@ -8,9 +8,6 @@ import Language.Haskell.TH.Syntax
 import System.Directory
 import System.Process.Typed
 
-$(do runIO
-       (do exists <- doesFileExist "app.js"
-           unless exists (writeFile "app.js" ""))
-     qAddDependentFile "app.js"
-     runIO (do runProcess_ (proc "sh" ["bundle.sh"]))
-     pure [])
+$(runIO
+    (do runProcess_ (proc "sh" ["bundle.sh"])
+        pure []))
--- inflex-server/app/Main.hs
+++ inflex-server/app/Main.hs
@@ -3,17 +3,24 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE QuasiQuotes #-}
 
+import qualified Paths_inflex_client
 import Yesod
 
 data App = App
 instance Yesod App
 
 mkYesod "App" [parseRoutes|
+  /appjs AppJsR GET
   / AppR GET
 |]
 
-getAppR :: Handler Html
+getAppR :: Handler TypedContent
 getAppR = sendFile "text/html" "index.html"
+
+getAppJsR :: Handler TypedContent
+getAppJsR = do
+  fp <- liftIO (Paths_inflex_client.getDataFileName "app.js")
+  sendFile "application/javascript" fp
 
 main :: IO ()
 main = warpEnv App
