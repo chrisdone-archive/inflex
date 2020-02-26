45dea59d2169296b3be131cd6342419e6fae19c7
Chris Done <chrisdone@gmail.com>
2020-02-26 18:24:49 +0000

    Update doc

--- inflex-client/src/Inflex.Doc.purs
+++ inflex-client/src/Inflex.Doc.purs
@@ -30,12 +30,30 @@ import Effect.Class.Console (log)
 --
 -- 1. Generate a UUID for each decl-which-is-a-component in-a-document.
 -- 2. Updates to each dec are routed to the same UUID, same component.
--- 3. A field in the input argument to dec (early on) can be a SHA512
+-- 3. A field in the input argument to dec component can be a SHA512
 --    of the whole dec, causing a very fast equality comparison via
 --    manual implementation of input handler in the Dec component.
 
+-- NOTABLE: It's more expensive to delete/recreate a decl, than to
+-- re-render and have the vdom differ update the small part that
+-- changed.
+
 -- IMPORTANT QUESTION: How does Halogen's components know when NOT to
 -- re-render? Is it on H.modify?
+-- ANSWER: It's H.modify (or H.put) <https://discourse.purescript.org/t/performance-in-halogen-components/456>
+
+--
+-- So we have 3 levels of avoiding work:
+--
+-- 1. Don't recreate components.
+-- 2. Don't update component state if the SHA256 hasn't changed.
+-- 3. Don't recreate the DOM, just modify in place via the VDOM.
+--
+-- Further microptimizations (depends on the size of the doc):
+--
+-- 1. Have the server only send those UUIDs that actually changed: it
+--    knows the input, and knows the output. Easy to diff. This would
+--    be 4 levels of avoiding work.
 
 component :: forall q i o m. MonadEffect m =>  H.Component HH.HTML q i o m
 component =
