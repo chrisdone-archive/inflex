d6ac15aea7be6db78827bd52f00c5c6417e2c16d
Chris Done <chrisdone@gmail.com>
2020-02-28 15:04:04 +0000

    Parse request input

--- inflex-server/app/Main.hs
+++ inflex-server/app/Main.hs
@@ -1,15 +1,26 @@
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE QuasiQuotes #-}
 
-import Yesod
+import           Data.Aeson
+import           Data.HashMap.Strict (HashMap)
+import qualified Data.HashMap.Strict as HM
+import           Data.Text (Text)
+import qualified Data.Text as T
+import           GHC.Generics
+import           Yesod
 
 data App = App
 instance Yesod App
 
 mkYesod "App" [parseRoutes|
   /appjs AppJsR GET
+  /api/refresh RefreshR POST
   / AppR GET
 |]
 
@@ -18,6 +29,32 @@ getAppR = sendFile "text/html" "index.html"
 
 getAppJsR :: Handler TypedContent
 getAppJsR = sendFile "application/javascript" "../inflex-client/app.js"
+
+data DecOut = DecOut
+  { name :: Text
+  , rhs :: Text
+  , result :: Text
+  } deriving (Generic)
+instance ToJSON DecOut where
+  toJSON DecOut {name, rhs, result} =
+    object ["name" .= name, "rhs" .= rhs, "result" .= result]
+
+data DecIn = DecIn
+  { name :: Text
+  , rhs :: Text
+  } deriving (Generic)
+instance FromJSON DecIn where
+  parseJSON j = do
+    o <- parseJSON j
+    DecIn <$> o .: "name" <*> o .: "rhs"
+
+postRefreshR :: Handler TypedContent
+postRefreshR =
+  selectRep
+    (provideRep
+       (do json :: HashMap Text DecIn <- requireCheckJsonBody
+           -- TODO: Evaluate the decls as a Duet module.
+           pure Null))
 
 main :: IO ()
 main = warpEnv App
