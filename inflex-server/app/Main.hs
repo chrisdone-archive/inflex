c9d308203d42b0a00ab9d2928ce4b6a537beff47
Chris Done <chrisdone@gmail.com>
2020-02-29 10:46:29 +0000

    Import Duet code

--- inflex-server/app/Main.hs
+++ inflex-server/app/Main.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DuplicateRecordFields #-}
@@ -14,6 +17,23 @@ import           Data.Text (Text)
 import qualified Data.Text as T
 import           GHC.Generics
 import           Yesod
+import           Control.Monad.Catch (catch, MonadThrow)
+import           Control.Monad.Logger
+import           Control.Monad.Supply
+import           Control.Monad.Writer
+import qualified Data.Map.Strict as M
+import           Data.Semigroup ((<>))
+import           Duet.Context
+import           Duet.Errors
+import           Duet.Infer
+import           Duet.Parser
+import           Duet.Printer
+import           Duet.Renamer
+import           Duet.Setup
+import           Duet.Simple
+import           Duet.Stepper
+import           Duet.Types
+import           System.IO
 
 data App = App
 instance Yesod App
@@ -58,3 +78,89 @@ postRefreshR =
 
 main :: IO ()
 main = warpEnv App
+
+--------------------------------------------------------------------------------
+-- Duet helpers
+
+data Run = Run
+  { runInputFile :: FilePath
+  , runMainIs :: String
+  , runConcise :: Bool
+  , runNumbered :: Bool
+  , runSteps :: Maybe Integer
+  , runHideSteps :: Bool
+  } deriving (Show)
+
+runProgram :: Run -> IO ()
+runProgram run@Run {..} = do
+  catch
+    (catch
+       (runNoLoggingT
+          (evalSupplyT
+             (do decls <- liftIO (parseFile runInputFile)
+                 (binds, ctx) <- createContext decls
+                 things <-
+                   execWriterT
+                     (runStepperIO
+                        run
+                        runSteps
+                        ctx
+                        (fmap (fmap typeSignatureA) binds)
+                        runMainIs)
+                 pure things)
+             [1 ..]))
+       (putStrLn . displayContextException))
+    (putStrLn . displayParseException)
+
+-- | Run the substitution model on the code.
+runStepperIO ::
+     forall m. (MonadSupply Int m, MonadThrow m, MonadIO m)
+  => Run
+  -> Maybe Integer
+  -> Context Type Name Location
+  -> [BindGroup Type Name Location]
+  -> String
+  -> m ()
+runStepperIO Run {..} maxSteps ctx bindGroups' i = do
+  e0 <- lookupNameByString i bindGroups'
+  loop 1 "" e0
+  where
+    loop :: Integer -> String -> Expression Type Name Location -> m ()
+    loop count lastString e = do
+      e' <- expandSeq1 ctx bindGroups' e
+      let string = printExpression (defaultPrint) e
+      when
+        (string /= lastString && not runHideSteps)
+        (if cleanExpression e || not runConcise
+           then liftIO
+                  (putStrLn
+                     ((if runNumbered
+                         then "[" ++ show count ++ "]\n"
+                         else "") ++
+                      printExpression defaultPrint e))
+           else pure ())
+      e'' <- pure e'
+      if (fmap (const ()) e'' /= fmap (const ()) e) &&
+         case maxSteps of
+           Just top -> count < top
+           Nothing -> True
+        then do
+          newE <-
+            renameExpression
+              (contextSpecials ctx)
+              (contextScope ctx)
+              (contextDataTypes ctx)
+              e''
+          loop (count + 1) string newE
+        else pure ()
+
+-- | Filter out expressions with intermediate case, if and immediately-applied lambdas.
+cleanExpression :: Expression Type i l -> Bool
+cleanExpression =
+  \case
+    CaseExpression {} -> False
+    IfExpression {} -> False
+    e0
+      | (LambdaExpression {}, args) <- fargs e0 -> null args
+    ApplicationExpression _ f x -> cleanExpression f && cleanExpression x
+    _ -> True
