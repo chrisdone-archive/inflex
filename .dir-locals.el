e6feaac09816d91a17049e132dd4165a7ccc90c0
Chris Done <chris@skyabove.io>
2020-04-04 11:49:03 +0100

    Remove

--- .dir-locals.el
+++
@@ -1,5 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((haskell-mode
-  (intero-targets "inflex-server:lib" "inflex-server:exe:inflex-server" "inflex-server:test:inflex-types")))
