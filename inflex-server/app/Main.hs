d65cc028585e7e7f78dbff57e47a8f3b846fb3ba
Chris Done <chris@skyabove.io>
2020-04-14 11:23:30 +0100

    Todos [skip ci]

--- inflex-server/app/Main.hs
+++ inflex-server/app/Main.hs
@@ -31,6 +31,8 @@ main = do
             (do runMigration migrateAll
                 manualMigration))
        app <- liftIO (toWaiAppPlain App {appPool = pool})
+       -- TODO: Middleware for password-protecting the site
+       -- TODO: Middleware for blocking non-load-balancer requests
        liftIO (run port (gzip def {gzipFiles = GzipCompress} app)))
 
 withDBPool ::
