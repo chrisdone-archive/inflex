a1713f7b165d20dfc2427a82eeb326a1ce38ae13
Chris Done <chris@skyabove.io>
2020-05-24 14:44:21 +0100

    More pending

--- inflex-lang/test/GenerateSpec.hs
+++ inflex-lang/test/GenerateSpec.hs
@@ -165,3 +165,108 @@ spec = do
                          })
                    ]
              })))
+  it
+    "Apply"
+    (do pending
+        shouldBe
+          (generateText "" "(\\x->x)123")
+          (Right
+             (HasConstraints
+                { classes =
+                    Seq.fromList
+                      [ ClassConstraint
+                          { className = FromIntegerClassName
+                          , types =
+                              VariableType
+                                (TypeVariable
+                                   { location = LambdaBodyCursor ExpressionCursor
+                                   , prefix = IntegeryPrefix
+                                   , index = 1
+                                   }) :|
+                              []
+                          , location = LambdaBodyCursor ExpressionCursor
+                          }
+                      ]
+                , thing =
+                    LambdaExpression
+                      (Lambda
+                         { location = ExpressionCursor
+                         , param =
+                             Param
+                               { location = LambdaParamCursor
+                               , name = ()
+                               , typ =
+                                   VariableType
+                                     (TypeVariable
+                                        { location = LambdaParamCursor
+                                        , prefix = LambdaParameterPrefix
+                                        , index = 0
+                                        })
+                               }
+                         , body =
+                             LiteralExpression
+                               (IntegerLiteral
+                                  (Integery
+                                     { location = LambdaBodyCursor ExpressionCursor
+                                     , integer = 123
+                                     , typ =
+                                         VariableType
+                                           (TypeVariable
+                                              { location =
+                                                  LambdaBodyCursor ExpressionCursor
+                                              , prefix = IntegeryPrefix
+                                              , index = 1
+                                              })
+                                     }))
+                         , typ =
+                             ApplyType
+                               (TypeApplication
+                                  { function =
+                                      ApplyType
+                                        (TypeApplication
+                                           { function =
+                                               ConstantType
+                                                 (TypeConstant
+                                                    { location = ExpressionCursor
+                                                    , name = FunctionTypeName
+                                                    })
+                                           , argument =
+                                               VariableType
+                                                 (TypeVariable
+                                                    { location = LambdaParamCursor
+                                                    , prefix =
+                                                        LambdaParameterPrefix
+                                                    , index = 0
+                                                    })
+                                           , location = ExpressionCursor
+                                           })
+                                  , argument =
+                                      VariableType
+                                        (TypeVariable
+                                           { location =
+                                               LambdaBodyCursor ExpressionCursor
+                                           , prefix = IntegeryPrefix
+                                           , index = 1
+                                           })
+                                  , location = ExpressionCursor
+                                  })
+                         })
+                , mappings =
+                    M.fromList
+                      [ ( ExpressionCursor
+                        , SourceLocation
+                            { start = SourcePos {line = 1, column = 1, name = ""}
+                            , end = SourcePos {line = 1, column = 8, name = ""}
+                            })
+                      , ( LambdaBodyCursor ExpressionCursor
+                        , SourceLocation
+                            { start = SourcePos {line = 1, column = 5, name = ""}
+                            , end = SourcePos {line = 1, column = 8, name = ""}
+                            })
+                      , ( LambdaParamCursor
+                        , SourceLocation
+                            { start = SourcePos {line = 1, column = 2, name = ""}
+                            , end = SourcePos {line = 1, column = 3, name = ""}
+                            })
+                      ]
+                })))
