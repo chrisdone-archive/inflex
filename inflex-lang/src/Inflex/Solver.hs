63a58b69ca7c7e2a6f0ebdb2072f01fdfa578682
Chris Done <chris@skyabove.io>
2020-05-30 16:05:23 +0100

    Substition in type

--- inflex-lang/src/Inflex/Solver.hs
+++ inflex-lang/src/Inflex/Solver.hs
@@ -1,3 +1,7 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
@@ -6,14 +10,15 @@
 
 module Inflex.Solver where
 
-import           Data.Bifunctor
-import           Data.Functor.Identity
-import           Data.List.NonEmpty (NonEmpty(..))
-import           Data.Map.Strict (Map)
-import           Data.Sequence (Seq)
-import           Data.Text (Text)
-import           Inflex.Generator
-import           Inflex.Types
+import Data.Bifunctor
+import Data.Functor.Identity
+import Data.List
+import Data.List.NonEmpty (NonEmpty(..))
+import Data.Map.Strict (Map)
+import Data.Sequence (Seq)
+import Data.Text (Text)
+import Inflex.Generator
+import Inflex.Types
 
 --------------------------------------------------------------------------------
 -- Solver types
@@ -59,7 +64,8 @@ solveText fp text = do
     (let thing = runIdentity (runSolver (expressionSolver expression))
       in pure (IsSolved {thing, mappings, classes}))
 
-solveConstraints :: Seq EqualityConstraint -> Either (NonEmpty SolveError) (Seq Substitution)
+solveConstraints ::
+     Seq EqualityConstraint -> Either (NonEmpty SolveError) (Seq Substitution)
 solveConstraints _ = Right mempty
 
 --------------------------------------------------------------------------------
@@ -67,3 +73,22 @@ solveConstraints _ = Right mempty
 
 expressionSolver :: Expression Generated -> Solver (Expression Solved)
 expressionSolver = error "TODO"
+
+--------------------------------------------------------------------------------
+-- Substitution
+
+substituteType :: [Substitution] -> Type Generated -> Type Generated
+substituteType substitutions = go
+  where
+    go =
+      \case
+        typ@ConstantType {} -> typ
+        ApplyType TypeApplication {function, argument, ..} ->
+          ApplyType
+            TypeApplication {function = go function, argument = go argument, ..}
+        typ@(VariableType typeVariable :: Type Generated) ->
+          case find
+                 (\Substitution {before} -> before == typeVariable)
+                 substitutions of
+            Just Substitution {after} -> after
+            Nothing -> typ
