a1b02b1c40cee40049c238429839590132323a2e
Chris Done <chris@skyabove.io>
2020-06-03 16:17:11 +0100

    Add valid type generation

--- inflex-lang/test/Instances.hs
+++ inflex-lang/test/Instances.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
@@ -26,6 +27,80 @@ instance GenUnchecked (Type Generated) where
 
 deriving instance Generic (TypeApplication Generated)
 instance GenUnchecked (TypeApplication Generated)
+
+instance Validity (Type Generated) where
+  validate = mempty
+
+instance GenValid (Type Generated) where
+  genValid = genType
+
+genType :: Gen (Type Generated)
+genType = do
+  size <- getSize
+  oneof
+    (take
+       (if size > 1
+          then 3
+          else 2)
+       [gen0Type, genApp1Type, scale (max 0 . pred) genApp2Type])
+  where
+    gen0Type =
+      oneof
+        [ pure
+            (ConstantType
+               TypeConstant
+                 {location = ExpressionCursor, name = IntegerTypeName})
+        , pure
+            (ConstantType
+               TypeConstant {location = ExpressionCursor, name = TextTypeName})
+        , do index <- choose (1, 10)
+             pure
+               (VariableType
+                  TypeVariable
+                    { location = ExpressionCursor
+                    , prefix = IntegeryPrefix
+                    , index = fromIntegral (index :: Integer)
+                    , kind = TypeKind
+                    })
+        ]
+    gen1Type =
+      pure
+        (ConstantType
+           TypeConstant {location = ExpressionCursor, name = OptionTypeName})
+    gen2Type =
+      pure
+        (ConstantType
+           TypeConstant {location = ExpressionCursor, name = FunctionTypeName})
+    genApp1Type = do
+      func <- gen1Type
+      arg <- genType
+      pure
+        (ApplyType
+           TypeApplication
+             { function = func
+             , argument = arg
+             , location = ExpressionCursor
+             , kind = TypeKind
+             })
+    genApp2Type = do
+      func <- gen2Type
+      arg1 <- genType
+      arg2 <- genType
+      pure
+        (ApplyType
+           TypeApplication
+             { function =
+                 ApplyType
+                   TypeApplication
+                     { function = func
+                     , argument = arg1
+                     , location = ExpressionCursor
+                     , kind = TypeKind
+                     }
+             , argument = arg2
+             , location = ExpressionCursor
+             , kind = TypeKind
+             })
 
 deriving instance Generic (TypeVariable Generated)
 instance GenUnchecked (TypeVariable Generated)
