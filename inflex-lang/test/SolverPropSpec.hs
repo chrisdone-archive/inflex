8648c4df84fe587285ddd85147064689b666d9f9
Chris Done <chris@skyabove.io>
2020-06-03 10:02:02 +0100

    Printer for solver prop

--- inflex-lang/test/SolverPropSpec.hs
+++ inflex-lang/test/SolverPropSpec.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 {-# LANGUAGE StandaloneDeriving #-}
 
@@ -5,8 +8,12 @@
 
 module SolverPropSpec where
 
-import Language.Haskell.Interpreter
-import Test.Hspec
+import           Data.Foldable
+import           Data.Sequence (Seq)
+import qualified Data.Sequence as Seq
+import           Inflex.Types
+import           Language.Haskell.Interpreter
+import           Test.Hspec
 
 --------------------------------------------------------------------------------
 -- Test suite
@@ -23,6 +30,39 @@ spec =
                  typeOf
                    "undefined :: (t ~ (->) a a, (->) a a ~ (->) (Maybe b) (Maybe Integer)) => t"))
           (Right "Maybe Integer -> Maybe Integer"))
+
+--------------------------------------------------------------------------------
+-- Rendering to Haskell string
+
+typeHsString :: Type Generated -> String
+typeHsString = toList . typeHs
+
+typeHs :: Type Generated -> Seq Char
+typeHs =
+  \case
+    VariableType typeVariable -> typeVariableHs typeVariable
+    ConstantType typeConstant -> typeConstantHs typeConstant
+    ApplyType TypeApplication {function, argument} ->
+      case function of
+        ApplyType TypeApplication {function = ConstantType TypeConstant {name = FunctionTypeName}} ->
+          typeHs function <> Seq.fromList " -> " <> typeHs argument
+        _ -> typeHs function <> Seq.fromList " " <> paren (typeHs argument)
+      where paren x =
+              if any (== ' ') (toList x)
+                then Seq.fromList "(" <> x <> Seq.fromList ")"
+                else x
+
+typeVariableHs :: TypeVariable Generated -> Seq Char
+typeVariableHs TypeVariable {index} = Seq.fromList (show index)
+
+typeConstantHs :: TypeConstant Generated -> Seq Char
+typeConstantHs TypeConstant {name=n} =
+  Seq.fromList
+    (case n of
+       FunctionTypeName -> "(->)"
+       IntegerTypeName -> "Integer"
+       TextTypeName -> "Text"
+       OptionTypeName -> "Maybe")
 
 --------------------------------------------------------------------------------
 -- Orphans
