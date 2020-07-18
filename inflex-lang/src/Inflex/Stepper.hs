42cdaf1c2260ae0fdc5ff2702d8716c56a651b61
Chris Done <chris@skyabove.io>
2020-07-18 17:07:56 +0100

    Basic stepping of integer operations

--- inflex-lang/src/Inflex/Stepper.hs
+++ inflex-lang/src/Inflex/Stepper.hs
@@ -1,14 +1,21 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NamedFieldPuns #-}
+
 -- | Step the code.
 
 module Inflex.Stepper where
 
-import           Data.Bifunctor
-import           Data.Map.Strict (Map)
-import           Data.Text (Text)
-import           Inflex.Resolver
-import           Inflex.Types
+import Control.Monad.Reader
+import Control.Monad.State
+import Data.Bifunctor
+import Data.Map.Strict (Map)
+import Data.Text (Text)
+import Inflex.Resolver
+import Inflex.Types
 
 --------------------------------------------------------------------------------
 -- Types
@@ -18,9 +25,23 @@ data ResolveStepError
   | StepError StepError
   deriving (Show, Eq)
 
-data StepError =
-  NotInScope Hash
+data StepError
+  = NotInScope Hash
+  | InvalidPrimOpOperands
   deriving (Show, Eq)
+
+data Stepped
+  = Continue
+  | Stepped
+
+newtype Step a = Step
+  { unStep :: ReaderT (Map Hash (Expression Resolved)) (StateT Stepped (Either StepError)) a
+  } deriving ( Functor
+             , Monad
+             , Applicative
+             , MonadState Stepped
+             , MonadReader (Map Hash (Expression Resolved))
+             )
 
 --------------------------------------------------------------------------------
 -- Main entry points
@@ -30,24 +51,86 @@ stepText ::
   -> Map Hash (Expression Resolved)
   -> FilePath
   -> Text
-  -> Either ResolveStepError [Expression Resolved]
+  -> Either ResolveStepError (Expression Resolved)
 stepText schemes values fp text = do
   IsResolved {thing} <- first ResolverErrored (resolveText schemes fp text)
-  first StepError (stepExpression values thing)
+  first
+    StepError
+    (evalStateT
+       (runReaderT (unStep (stepExpression thing)) values)
+       Continue)
 
 --------------------------------------------------------------------------------
 -- Steppers
 
 stepExpression ::
-     Map Hash (Expression Resolved)
+     Expression Resolved
+  -> Step (Expression Resolved)
+stepExpression expression = do
+  stepped <- get
+  case stepped of
+    Stepped -> pure expression
+    Continue ->
+      case expression of
+        ApplyExpression apply -> stepApply apply
+        InfixExpression infix' -> stepInfix infix'
+        LiteralExpression {} -> pure expression
+        LambdaExpression {} -> pure expression
+        VariableExpression {} -> pure expression
+        GlobalExpression {} -> pure expression
+        LetExpression {} -> pure expression
+
+stepApply :: Apply Resolved -> Step (Expression Resolved)
+stepApply Apply {..} = do
+  function' <- stepExpression function
+  argument' <- stepExpression argument
+  stepped <- get
+  case stepped of
+    Stepped ->
+      pure
+        (ApplyExpression Apply {function = function', argument = argument', ..})
+    Continue ->
+      pure
+        (ApplyExpression Apply {function = function', argument = argument', ..})
+
+stepInfix :: Infix Resolved -> Step (Expression Resolved)
+stepInfix Infix {..} = do
+  global' <- stepExpression global
+  left' <- stepExpression left
+  right' <- stepExpression right
+  stepped <- get
+  case stepped of
+    Stepped ->
+      pure
+        (InfixExpression
+           Infix {global = global', left = left', right = right', ..})
+    Continue ->
+      case global' of
+        ApplyExpression Apply { function = GlobalExpression Global {name = NumericBinOpGlobal {}}
+                              , argument = GlobalExpression Global {name = InstanceGlobal (IntegerOpInstance numericBinOp)}
+                              } -> stepIntegerOp numericBinOp left' right'
+        _ -> error ("stepInfix: " ++ show global')
+
+stepIntegerOp ::
+     NumericBinOp
   -> Expression Resolved
-  -> Either StepError [Expression Resolved]
-stepExpression _globals =
-  \case
-     e@LiteralExpression {} -> pure [e]
-     e@LambdaExpression {} -> pure [e]
-     e@ApplyExpression {} -> pure [e]
-     e@VariableExpression {} -> pure [e]
-     e@GlobalExpression {} -> pure [e]
-     e@LetExpression {} -> pure [e]
-     e@InfixExpression {} -> pure [e]
+  -> Expression Resolved
+  -> Step (Expression Resolved)
+stepIntegerOp numericBinOp left' right' =
+  case (left', right') of
+    (LiteralExpression (NumberLiteral Number {number = IntegerNumber left, typ}), LiteralExpression (NumberLiteral Number {number = IntegerNumber right})) -> do
+      pure
+        (LiteralExpression
+           (NumberLiteral
+              Number
+                { number =
+                    IntegerNumber
+                      (case numericBinOp of
+                         AddOp -> left + right
+                         SubtractOp -> left - right
+                         MulitplyOp -> left * right
+                         DivideOp -> div left right)
+                , location = SteppedCursor
+                , ..
+                }))
+    _ -> Step (lift (lift (Left InvalidPrimOpOperands)))
--- inflex-lang/test/StepSpec.hs
+++ inflex-lang/test/StepSpec.hs
@@ -9,12 +9,16 @@ import           Inflex.Stepper
 import           RIO (textDisplay)
 import           Test.Hspec
 
-stepTextly :: Text -> Either ResolveStepError [Text]
-stepTextly text = fmap (fmap textDisplay) (stepText mempty mempty "" text)
+stepTextly :: Text -> Either ResolveStepError Text
+stepTextly text = fmap textDisplay (stepText mempty mempty "" text)
 
 spec :: SpecWith ()
 spec =
   describe
     "Single expressions"
-    (do it "1" (shouldBe (stepTextly "1 :: Integer") (Right ["1"]))
-        )
+    (do it "6" (shouldBe (stepTextly "6 :: Integer") (Right "6"))
+        it "6 + 3" (shouldBe (stepTextly "6 :: Integer + 3 :: Integer") (Right "9"))
+        it "6 * 3" (shouldBe (stepTextly "6 :: Integer * 3 :: Integer") (Right "18"))
+        it "6 / 3" (shouldBe (stepTextly "6 :: Integer / 3 :: Integer") (Right "2"))
+        it "6 - 3" (shouldBe (stepTextly "6 :: Integer - 3 :: Integer") (Right "3"))
+        it "6 - 3 * 3" (shouldBe (stepTextly "6 :: Integer - 3 :: Integer * 3 :: Integer") (Right "-3")))
