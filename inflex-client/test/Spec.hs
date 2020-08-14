0f685ef284d4a40bc327e9882de82d218889a966
Chris Done <chris@skyabove.io>
2020-08-14 15:15:11 +0100

    Use genvalidity to generate whole structures

--- inflex-client/test/Spec.hs
+++ inflex-client/test/Spec.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 
@@ -7,6 +8,9 @@ module Main where
 
 import           Data.Aeson
 import qualified Data.ByteString.Lazy.Char8 as L8
+import           Data.GenValidity
+import           Data.GenValidity.Text ()
+import           Data.GenValidity.Vector ()
 import           Data.Vector (Vector)
 import qualified Data.Vector as V
 import           GHC.Generics
@@ -14,6 +18,7 @@ import           System.Directory
 import           System.Process.Typed
 import           Test.Hspec
 import           Test.QuickCheck
+import           Test.Validity
 
 main :: IO ()
 main = do
@@ -48,49 +53,43 @@ main = do
                 ())
            it
              "node run: roundtrip: MyRecord"
-             (do property
-                   (\i ->
-                      let r = MyRecord {a = i}
-                       in shouldReturn
-                            (fmap
-                               decode
-                               (readProcessStdout_
-                                  (proc
-                                     "/home/chris/qjs"
-                                     [ "app.js"
-                                     , "MyRecord"
-                                     , L8.unpack (encode r)
-                                     ])))
-                            (pure r)))
+             (forAllUnchecked
+                @MyRecord
+                (\r ->
+                   shouldReturn
+                     (fmap
+                        decode
+                        (readProcessStdout_
+                           (proc
+                              "/home/chris/qjs"
+                              ["app.js", "MyRecord", L8.unpack (encode r)])))
+                     (pure r)))
            it
              "node run: roundtrip: MyRecord2"
-             (do property
-                   (\i ->
-                      let r =
-                            MyRecord2
-                              { b = i
-                              , myrec = MyRecord {a = i * 2}
-                              , arr =
-                                  V.replicate (mod i 10) (MyRecord {a = i * 3})
-                              }
-                       in shouldReturn
-                            (fmap
-                               decode
-                               (readProcessStdout_
-                                  (proc
-                                     "/home/chris/qjs"
-                                     [ "app.js"
-                                     , "MyRecord2"
-                                     , L8.unpack (encode r)
-                                     ])))
-                            (pure r)))))
+             (forAllUnchecked
+                @MyRecord2
+                (\r ->
+                   shouldReturn
+                     (fmap
+                        decode
+                        (readProcessStdout_
+                           (proc
+                              "/home/chris/qjs"
+                              ["app.js", "MyRecord2", L8.unpack (encode r)])))
+                     (pure r)))))
 
 data MyRecord = MyRecord { a :: Int }
  deriving (Generic, Show, Eq)
 instance FromJSON MyRecord
 instance ToJSON MyRecord
+instance GenValid MyRecord
+instance Validity MyRecord
+instance GenUnchecked MyRecord
 
 data MyRecord2 = MyRecord2 { b :: Int, myrec :: MyRecord, arr :: Vector MyRecord }
  deriving (Generic, Show, Eq)
 instance FromJSON MyRecord2
 instance ToJSON MyRecord2
+instance GenValid MyRecord2
+instance Validity MyRecord2
+instance GenUnchecked MyRecord2
