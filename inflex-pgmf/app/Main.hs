3bc33012604d688e51f4ef29953f8bfa7916729e
Chris Done <chris@skyabove.io>
2020-08-17 15:37:58 +0100

    Whip it

--- inflex-pgmf/app/Main.hs
+++ inflex-pgmf/app/Main.hs
@@ -4,7 +4,7 @@
 
 module Main where
 
-import           Data.Either
+import           Control.Concurrent.Async
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import           System.Environment
@@ -17,9 +17,16 @@ main = do
       text <- T.readFile inputf
       let ls = T.lines text
           content = T.unlines (drop 1 ls)
-      T.writeFile outputf "module Inflex.Shared.Spec where"
-      hs <- T.readFile "templates/Shared.hs"
-      T.writeFile "../inflex-server/src/Inflex/Shared.hs" (T.replace "$types" content hs)
-      purs <- T.readFile "templates/Shared.purs"
-      T.writeFile "../inflex-client/src/Inflex/Shared.purs" (T.replace "$types" content purs)
+      T.writeFile outputf "module Inflex.Shared.Schema where"
+      concurrently_
+        (do hs <- T.readFile "templates/Shared.hs"
+            T.writeFile
+              "../inflex-server/src/Inflex/Shared.hs"
+              (T.replace "$types" content hs)
+            )
+        (do purs <- T.readFile "templates/Shared.purs"
+            T.writeFile
+              "../inflex-client/src/Inflex/Shared.purs"
+              (T.replace "$types" content purs)
+            )
     _ -> error "bad arguments"
