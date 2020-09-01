3c1222b53d90c35120aed8d20310260bd99b412d
Chris Done <chris@skyabove.io>
2020-09-01 13:30:35 +0100

    Drop unused Inflex.Json

--- inflex-client/src/Inflex/Json.hs
+++
@@ -1,8 +0,0 @@
--- |
-
-module Inflex.Json where
-
-import Foreign.Generic (class Decode, class Encode, SumEncoding, defaultOptions, genericDecode, genericEncode)
-
-opts :: { fieldTransform :: String -> String , sumEncoding :: SumEncoding , unwrapSingleArguments :: Boolean , unwrapSingleConstructors :: Boolean}
-opts = defaultOptions { unwrapSingleConstructors = true }
