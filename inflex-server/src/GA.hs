a36d02f3857cce6f0eece4bf0976df4ec0184f3d
Chris Done <chris@skyabove.io>
2020-09-12 13:09:18 +0100

    Submit more info to GA

--- inflex-server/src/GA.hs
+++ inflex-server/src/GA.hs
@@ -11,7 +11,9 @@
 module GA (submitGA) where
 
 import           Control.Concurrent
+import           Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as S8
+import qualified Data.ByteString.Lazy.Char8 as L8
 import           Data.Functor
 import           Data.Maybe
 import           Data.Text (Text)
@@ -30,37 +32,51 @@ submitGA = do
   lock <- fmap appGALock getYesod
   Config{gaUa} <- fmap appConfig getYesod
   uid <- makeGaUid
-  maybe (pure ()) (liftIO . void . forkIO . flip (send uid gaUa) lock) mroute
+  ip <- lookupHeader "x-real-ip"
+  mua <- lookupHeader "user-agent"
+  mref <- lookupHeader "referer"
+  maybe (pure ()) (liftIO . void . forkIO . flip (send mref mua ip uid gaUa) lock) mroute
 
 -- | Send to GA.
-send :: GA_UID -> GA_UA -> Route App -> MVar () -> IO ()
-send uid gaUa route = flip withMVar (const (sendIt uid gaUa route))
+send :: Maybe ByteString -> Maybe ByteString ->  Maybe ByteString ->   GA_UID -> GA_UA -> Route App -> MVar () -> IO ()
+send mref mua ip uid gaUa route = flip withMVar (const (sendIt mref mua ip uid gaUa route))
 
-sendIt :: GA_UID ->  GA_UA -> Route App -> IO ()
-sendIt uid gaUa route = do
+sendIt :: Maybe ByteString ->  Maybe ByteString ->  Maybe ByteString ->   GA_UID ->  GA_UA -> Route App -> IO ()
+sendIt mref mua mip uid gaUa route = do
   S8.putStrLn "Queue to send GA."
   case renderRouteForGA route of
     Just rendered -> do
       request <- fmap hydrate (parseRequest endpoint)
-      _response <- httpNoBody request
+      response <- httpLbs request
+      L8.putStrLn (getResponseBody response)
+      print params
       S8.putStrLn "Sent GA!"
       pure ()
-      where hydrate =
+      where params =
+              [ ("v", "1")
+              , ("t", "pageview")
+              , ("tid", T.encodeUtf8 (unGA_UA gaUa))
+              , ("dp", T.encodeUtf8 rendered)
+              , ("uid", sha256AsHexBS (unGA_UID uid))
+              , ("ds", "web")
+              , ("dh", "inflex.io")
+              , ("aip", "1")
+              , ("npa", "1")
+              ] <>
+              [("uip", ip) | Just ip <- [mip]] <>
+              [("ua", ua) | Just ua <- [mua]] <>
+              [("dr", dr) | Just dr <- [mref]]
+            hydrate =
               setRequestMethod "POST" .
-              setRequestSecure True .
-              setRequestBodyURLEncoded
-                [ ("v", "1")
-                , ("t", "pageview")
-                , ("tid", T.encodeUtf8 (unGA_UA gaUa))
-                , ("dp", T.encodeUtf8 rendered)
-                , ("uid", sha256AsHexBS (unGA_UID uid))
-                , ("ds", "web")
-                , ("dh", "inflex.io")
-                ]
+              (case mua of
+                 Just ua -> addRequestHeader "user-agent" ua
+                 Nothing -> id) .
+              setRequestSecure True . setRequestBodyURLEncoded params
     Nothing -> pure ()
 
 endpoint :: String
 endpoint = "https://www.google-analytics.com/collect"
+-- endpoint = "https://www.google-analytics.com/debug/collect"
 
 -- | Render a route for consumption by GA -- we avoid sending
 -- PII/personal info.
