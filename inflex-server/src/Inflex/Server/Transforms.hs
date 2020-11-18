76d49dbc390340b01521be85e9922638d14d954f
Chris Done <chris@skyabove.io>
2020-11-18 13:52:00 +0000

    Refactor transforms

--- inflex-server/src/Inflex/Server/Transforms.hs
+++ inflex-server/src/Inflex/Server/Transforms.hs
@@ -16,75 +16,34 @@ import qualified Inflex.Types as Field (Field(..))
 import qualified Inflex.Types as FieldE (FieldE(..))
 import           RIO
 
+-- TODO: Do something about errors occurring. And possibly type-check
+-- the result of the document afterwards?
+
 --------------------------------------------------------------------------------
 -- General dispatcher
 
 applyUpdateToDocument :: Shared.Update -> Shared.InputDocument1 -> Shared.InputDocument1
 applyUpdateToDocument (Shared.CellUpdate Shared.UpdateCell {uuid, update}) =
   case cmd of
-    Shared.NewFieldUpdate newField -> addNewFieldToDocument uuid path newField
-    Shared.DeleteFieldUpdate deleteField ->
-      deleteFieldToDocument uuid path deleteField
-    Shared.RenameFieldUpdate renameField ->
-      renameFieldToDocument uuid path renameField
+    Shared.NewFieldUpdate Shared.NewField {name = name0} ->
+      mapUuid uuid (addNewFieldInCode path (FieldName name0))
+    Shared.DeleteFieldUpdate Shared.DeleteField {name = name0} ->
+      mapUuid uuid (deleteFieldInCode path (FieldName name0))
+    Shared.RenameFieldUpdate Shared.RenameField {from,to=to0} ->
+      mapUuid uuid (renameFieldInCode path (FieldName from) (FieldName to0))
     Shared.CodeUpdate (Shared.Code code) -> mapUuid uuid (const code)
     Shared.AddToEndUpdate ->
       mapUuidPath uuid path Mapping {mapArray = addArrayItem, mapRecord = id}
     Shared.RemoveUpdate (Shared.Removal {index}) ->
-      mapUuidPath uuid path Mapping {mapArray = removeArrayItem index, mapRecord = id}
+      mapUuidPath
+        uuid
+        path
+        Mapping {mapArray = removeArrayItem index, mapRecord = id}
   where
     Shared.UpdatePath {path, update = cmd} = update
 
 --------------------------------------------------------------------------------
--- Handlers
-
-addNewFieldToDocument ::
-     Shared.UUID
-  -> Shared.DataPath
-  -> Shared.NewField
-  -> Shared.InputDocument1
-  -> Shared.InputDocument1
-addNewFieldToDocument uuid0 path Shared.NewField {name = name0} Shared.InputDocument1 {cells} =
-  Shared.InputDocument1 {cells = fmap apply cells}
-  where
-    apply same@Shared.InputCell1 {..} =
-      if uuid == uuid0
-        then Shared.InputCell1
-               {code = addNewFieldInCode path (FieldName name0) code, ..}
-        else same
-
-deleteFieldToDocument ::
-     Shared.UUID
-  -> Shared.DataPath
-  -> Shared.DeleteField
-  -> Shared.InputDocument1
-  -> Shared.InputDocument1
-deleteFieldToDocument uuid0 path Shared.DeleteField {name = name0} Shared.InputDocument1 {cells} =
-  Shared.InputDocument1 {cells = fmap apply cells}
-  where
-    apply same@Shared.InputCell1 {..} =
-      if uuid == uuid0
-        then Shared.InputCell1
-               {code = deleteFieldInCode path (FieldName name0) code, ..}
-        else same
-
-renameFieldToDocument ::
-     Shared.UUID
-  -> Shared.DataPath
-  -> Shared.RenameField
-  -> Shared.InputDocument1
-  -> Shared.InputDocument1
-renameFieldToDocument uuid0 path Shared.RenameField {from,to=to0} Shared.InputDocument1 {cells} =
-  Shared.InputDocument1 {cells = fmap apply cells}
-  where
-    apply same@Shared.InputCell1 {..} =
-      if uuid == uuid0
-        then Shared.InputCell1
-               {code = renameFieldInCode path (FieldName from) (FieldName to0) code, ..}
-        else same
-
--- TODO: Do something about errors occurring. And possibly type-check
--- the result of the document afterwards?
+-- Code updaters
 
 addNewFieldInCode :: Shared.DataPath -> FieldName -> Text -> Text
 addNewFieldInCode path0 name code =
@@ -132,12 +91,6 @@ addNewFieldInCode path0 name code =
                     ]
                 }
         e -> e
-
-withFields :: ([Field Parsed] -> [Field Parsed]) -> Array Parsed -> Array Parsed
-withFields f array@Array {typ}
-  | Just (ArrayType (RecordType (RowType (row@TypeRow {fields})))) <- typ =
-    array { typ = Just (ArrayType (RecordType (RowType row {fields = f fields}))) }
-withFields _f array = array
 
 renameFieldInCode :: Shared.DataPath -> FieldName -> FieldName -> Text -> Text
 renameFieldInCode path0 from to' code =
@@ -228,6 +181,12 @@ deleteFieldInCode path0 name0 code =
 
 --------------------------------------------------------------------------------
 -- Array
+
+withFields :: ([Field Parsed] -> [Field Parsed]) -> Array Parsed -> Array Parsed
+withFields f array@Array {typ}
+  | Just (ArrayType (RecordType (RowType (row@TypeRow {fields})))) <- typ =
+    array { typ = Just (ArrayType (RecordType (RowType row {fields = f fields}))) }
+withFields _f array = array
 
 addArrayItem :: Array Parsed -> Array Parsed
 addArrayItem array@Array {location, expressions} =
