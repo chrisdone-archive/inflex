14773663cb21f48b0cc825d777607fbd0f35356f
Chris Done <chris@skyabove.io>
2020-12-01 11:46:24 +0000

    Add #< #> #= to variants module

--- inflex-lang/src/Inflex/Variants.hs
+++ inflex-lang/src/Inflex/Variants.hs
@@ -11,6 +11,9 @@ module Inflex.Variants where
 import Inflex.Type
 import Inflex.Types
 
+--------------------------------------------------------------------------------
+-- Bool
+
 trueVariant :: StagedLocation Resolved -> Expression Resolved
 trueVariant location =
   VariantExpression
@@ -20,3 +23,21 @@ falseVariant :: StagedLocation Resolved -> Expression Resolved
 falseVariant location =
   VariantExpression
     Variant {location, typ = boolType location, tag = TagName "false", argument = Nothing}
+
+--------------------------------------------------------------------------------
+-- Ordering
+
+equalVariant :: StagedLocation Resolved -> Expression Resolved
+equalVariant location =
+  VariantExpression
+    Variant {location, typ = boolType location, tag = TagName "=", argument = Nothing}
+
+lessVariant :: StagedLocation Resolved -> Expression Resolved
+lessVariant location =
+  VariantExpression
+    Variant {location, typ = boolType location, tag = TagName "<", argument = Nothing}
+
+moreVariant :: StagedLocation Resolved -> Expression Resolved
+moreVariant location =
+  VariantExpression
+    Variant {location, typ = boolType location, tag = TagName ">", argument = Nothing}
