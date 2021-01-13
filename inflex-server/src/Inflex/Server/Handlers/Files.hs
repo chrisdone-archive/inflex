5f5f2905bab828f9f8b3b055e462af5fc826a65c
Chris Done <chris@skyabove.io>
2021-01-13 15:19:09 +0000

    gzip before storing

--- inflex-server/src/Inflex/Server/Handlers/Files.hs
+++ inflex-server/src/Inflex/Server/Handlers/Files.hs
@@ -10,8 +10,12 @@ module Inflex.Server.Handlers.Files
   ) where
 
 import           Control.Monad.Reader
+import           Data.ByteString (ByteString)
+import qualified Data.ByteString as S
+import qualified Data.ByteString.Lazy as L
 import           Data.Conduit
 import qualified Data.Conduit.Binary as CB
+import           Data.Conduit.Zlib
 import qualified Data.Text as T
 import           Data.Time
 import           Inflex.Server.App
@@ -20,7 +24,6 @@ import           Inflex.Server.Session
 import           Inflex.Server.Types
 import           Inflex.Server.Types.Sha256
 import           Path
-import           System.Directory
 import           Yesod
 import           Yesod.Core.Types as Yesod
 
@@ -31,20 +34,19 @@ postUploadFileR =
        fileInfo <- getFile
        if fileContentType fileInfo == "text/csv"
          then do
-           !hash <- hashFile fileInfo
+           !(hash, bytes) <- hashFile fileInfo
            !casfilename <- parseRelFile (sha256AsHexString hash)
            !dir <- fmap (uploadsDir . appConfig) getYesod
            let casPath = toFilePath (dir </> casfilename)
-           liftIO $ fileMove fileInfo casPath
+           liftIO (S.writeFile casPath bytes)
            now <- liftIO getCurrentTime
-           bytes <- liftIO (getFileSize casPath)
            runDB
              (insert_
                 Model.File
                   { fileAccount = fromAccountID loginAccountId
                   , fileName = Yesod.fileName fileInfo
                   , fileCreated = now
-                  , fileBytes = fromIntegral bytes -- TODO: Make this conversion explicit.
+                  , fileBytes = fromIntegral (S.length bytes) -- TODO: Make this conversion explicit.
                   , fileHash = hash
                   , fileMime = fileContentType fileInfo
                   })
@@ -62,10 +64,10 @@ getFile = do
           T.pack (show (length files))
         ]
 
-hashFile :: MonadIO m => FileInfo -> m Sha256
+hashFile :: MonadIO m => FileInfo -> m (Sha256, ByteString)
 hashFile fileInfo =
   liftIO
-    (do !sha <-
+    (do !compressed <-
           runConduitRes
-            (fileSourceRaw fileInfo .| fmap sha256LazyByteString CB.sinkLbs)
-        pure sha)
+            (fileSourceRaw fileInfo .| gzip .| fmap L.toStrict CB.sinkLbs)
+        pure (sha256ByteString compressed, compressed))
