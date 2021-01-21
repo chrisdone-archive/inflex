35e62733be033b754a7840e7dd3474d66c9af225
Chris Done <chris@skyabove.io>
2021-01-21 15:21:19 +0000

    Drop dirtiness

--- inflex-client/src/Inflex/Components/Cell.purs
+++ inflex-client/src/Inflex/Components/Cell.purs
@@ -218,21 +218,22 @@ eval :: forall q i m. MonadEffect m =>  Command -> H.HalogenM State q i Output m
 eval =
   case _ of
     TriggerUpdatePath update -> do
-      H.modify_ (\(State s) -> State (s {dirty = true}))
+      -- H.modify_ (\(State s) -> State (s {dirty = true}))
       H.raise (UpdatePath update)
     CodeUpdate (Cell {name, code}) -> do
-      H.modify_ (\(State s) -> State (s {dirty = true}))
+      -- H.modify_ (\(State s) -> State (s {dirty = true}))
       H.raise (CellUpdate {name, code})
     SetCellFromInput cell@(Cell {hash, name}) -> do
-      State s0 <- H.get
-      let Cell cell0 = s0 . cell
-      if s0 . dirty || cell0 . hash /= hash
-        then do
-          {-if s0.dirty
-             then log ("Updating dirty cell " <> name)
-             else log ("Updating changed cell " <> name)-}
-          H.modify_ (\(State s) -> State (s {dirty = false, cell = cell}))
-        else pure unit {-log ("Ignoring unchanged clean cell " <> name)-}
+      -- State s0 <- H.get
+      -- let Cell cell0 = s0 . cell
+      -- if s0 . dirty || cell0 . hash /= hash
+      --   then do
+      --     if s0.dirty
+      --        then log ("Updating dirty cell " <> name)
+      --        else log ("Updating changed cell " <> name)
+
+      --   else pure unit {-log ("Ignoring unchanged clean cell " <> name)-}
+      H.modify_ (\(State s) -> State (s {dirty = false, cell = cell}))
     DeleteCell -> H.raise RemoveCell
     DragStarted (DragEvent' dragEvent) -> do
       H.liftEffect
