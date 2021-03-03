e35c7ce3b8fce36522142a41e151a1f2f042c1d7
Chris Done <chris@skyabove.io>
2021-03-03 15:00:11 +0000

    Temporarily enable upload

--- inflex-server/src/Inflex/Server/Handlers/Dashboard.hs
+++ inflex-server/src/Inflex/Server/Handlers/Dashboard.hs
@@ -149,8 +149,8 @@ getAppDashboardR =
                                toHtml
                                  (bytesShorthand (maxUploadSizeBytes config))
                                ". (This may be adjusted during the beta. Please contact us in the forum if you need something slightly larger than this for your testing.)"))
-                      p_ (strong_ "Currently disabled.")
-                      {-form_
+                      -- p_ (strong_ "Currently disabled.")
+                      form_
                         [ action_ (url UploadFileR)
                         , method_ "post"
                         , id_ "file-upload-form"
@@ -165,7 +165,7 @@ getAppDashboardR =
                               ]
                             button_
                               [class_ "new-document", id_ "file-select"]
-                              "Upload File")-}
+                              "Upload File")
                       if null files
                         then p_ "No files yet."
                         else div_
