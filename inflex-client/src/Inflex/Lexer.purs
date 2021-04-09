a1eb1b1d1940a5ec00686c7640f6cf76d8afcc33
Chris Done <chris@skyabove.io>
2021-04-09 15:45:02 +0100

    Include text in the lexer

--- inflex-client/src/Inflex/Lexer.purs
+++ inflex-client/src/Inflex/Lexer.purs
@@ -4,6 +4,7 @@ module Inflex.Lexer where
 
 foreign import lexer :: String -> Array {
     tag :: String,
+    text :: String,
     location :: {
       start :: { line :: Int, column :: Int },
       end :: { line :: Int, column :: Int }
