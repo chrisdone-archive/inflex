41042cf019a0438db9f021ee31110953089b0916
Chris Done <chris@skyabove.io>
2021-04-16 10:11:30 +0100

    Reify lexed tokens to PS values

--- inflex-client/src/Inflex/Lexer.purs
+++ inflex-client/src/Inflex/Lexer.purs
@@ -1,12 +1,51 @@
 -- | Fast lexer.
 
-module Inflex.Lexer where
+module Inflex.Lexer
+  ( lexString
+  , Token(..)
+  ) where
 
-foreign import lexer :: String -> Array {
+import Data.Array as Array
+import Data.Either (Either(..), either)
+import Data.Generic.Rep (class Generic)
+import Data.Generic.Rep.Show (genericShow)
+import Data.UUID
+import Effect (Effect)
+import Effect.Exception
+import Prelude
+
+
+--------------------------------------------------------------------------------
+-- Types
+
+data Token = UuidToken UUID | PrimToken String | MiscToken
+derive instance genericToken :: Generic Token _
+instance showToken :: Show Token where show x = genericShow x
+
+--------------------------------------------------------------------------------
+-- Functions
+
+lexString :: String -> Effect (Either Error (Array Token))
+lexString i =
+  try
+    (do toks <- lexer i
+        pure
+          (map
+             (\tagged ->
+                case tagged . tag of
+                  "uuid" -> UuidToken (UUID (tagged . text))
+                  "prim" -> PrimToken (tagged . text)
+                  _ -> MiscToken)
+             toks))
+
+--------------------------------------------------------------------------------
+-- Foreign
+
+foreign import lexer :: String -> Effect (Array {
     tag :: String,
     text :: String,
     location :: {
       start :: { line :: Int, column :: Int },
       end :: { line :: Int, column :: Int }
     }
-  }
+  })
