02871abb8f41868038db330d11828ce64bbbb18f
Chris Done <chris@skyabove.io>
2021-10-06 09:22:08 +0100

    Fix scan type

--- inflex-lang/src/Inflex/Type.hs
+++ inflex-lang/src/Inflex/Type.hs
@@ -312,7 +312,7 @@ functionScheme location =
         ((a .-> boolT) .-> ArrayType a .->
          maybeType ["any_empty"] location boolT)
     FromOkFunction -> mono (b .-> okishType BuiltIn c b .-> b)
-    ScanFunction -> mono (a .-> (a .-> e .-> e) .-> ArrayType a .-> e)
+    ScanFunction -> mono (a .-> (a .-> e .-> a) .-> ArrayType e .-> a)
     AccumFunction ->
       mono
         (a .->
