ade159ce613aedbb0327a76cb45b5a8362bf38fb
Chris Done <chris@skyabove.io>
2022-04-12 17:33:29 +0100

    Disable timer output for now

--- inflex-client/src/Timed.js
+++ inflex-client/src/Timed.js
@@ -1,9 +1,9 @@
 exports.timed = function(label){
   return function(thunk){
-    const t0 = performance.now();
+    // const t0 = performance.now();
     const v = thunk(null);
-    const t1 = performance.now();
-    console.log("%s: %oms", label, t1 - t0);
+    // const t1 = performance.now();
+    // console.log("%s: %oms", label, t1 - t0);
     return v;
   }
 }
